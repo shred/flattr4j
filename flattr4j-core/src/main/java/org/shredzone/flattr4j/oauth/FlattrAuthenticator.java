16797ec8031bf640102486fbd2b1d4fc9543faef
Richard Körber <github@shredzone.org>
2012-04-27 20:35:54 +0200

    Formatting

--- flattr4j-core/src/main/java/org/shredzone/flattr4j/oauth/FlattrAuthenticator.java
+++ flattr4j-core/src/main/java/org/shredzone/flattr4j/oauth/FlattrAuthenticator.java
@@ -227,9 +227,9 @@ public class FlattrAuthenticator {
     protected String buildScopeString() {
         StringBuilder sb = new StringBuilder();
 
-        if (scope.contains(Scope.FLATTR)) sb.append(" flattr");
-        if (scope.contains(Scope.THING)) sb.append(" thing");
-        if (scope.contains(Scope.EMAIL)) sb.append(" email");
+        if (scope.contains(Scope.FLATTR      )) sb.append(" flattr");
+        if (scope.contains(Scope.THING       )) sb.append(" thing");
+        if (scope.contains(Scope.EMAIL       )) sb.append(" email");
         if (scope.contains(Scope.EXTENDEDREAD)) sb.append(" extendedread");
 
         return sb.toString().trim();
